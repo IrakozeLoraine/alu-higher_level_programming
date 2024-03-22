3ac234fe87825e389adb0077ef8de6c7e7a7b3a7
mukezw <mukezwa@gmail.com>
2024-03-22 11:50:18 +0200

    fix: 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """A script that
-- fetches https://alx-intranet.hbtn.io/status.
+- fetches http://0.0.0.0:5050/status.
 - uses urlib package
 """
 import urllib.request
 
 
 if __name__ == '__main__':
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    with urllib.request.urlopen('http://0.0.0.0:5050/status') as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
